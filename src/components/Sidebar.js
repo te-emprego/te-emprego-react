645325246af87c23d0e962df5ee6fb007d7970db
danielbonifacio <contato@danielbonifacio.com.br>
2019-07-01 14:41:51 -0300

    base

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import WidgetWrapper from './WidgetWrapper'
+
+class Sidebar extends Component {
+  render() {
+    return(
+      <WidgetWrapper>
+        Sidebar
+      </WidgetWrapper>
+    )
+  }
+}
+
+export default Sidebar
